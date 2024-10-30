f2fdea0fba74919ef8e0170a0e8df4d5d7e66614
JPNoman <jpfnoman@gmail.com>
2024-10-30 14:34:41 -0300

    background

--- jogo.py
+++ jogo.py
@@ -8,6 +8,8 @@ pygame.init()
 window = pygame.display.set_mode((600, 300))
 pygame.display.set_caption('Pokémon Beat em Up')
 
+image = pygame.image.load('assets/background.png').convert()
+image = pygame.transform.scale(image, (600, 300))
 # Loop de jogo
 
 game = True
@@ -17,9 +19,13 @@ while game:
             game = False
         if event.type == pygame.KEYUP:
             game = False
-    window.fill((255, 255, 255))
+    # window.fill((255, 255, 255))
     pygame.display.update()
 
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(image, (0, 0))
+    pygame.display.update()  # Mostra o novo frame para o jogador
 # Fecha o jogo
 
 pygame.quit()
