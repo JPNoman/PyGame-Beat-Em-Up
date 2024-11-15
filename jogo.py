fc8a040e58d5c600e56696bcef0fffda02ae9c42
LuizAssaf <felipe.h.assaf@gmail.com>
2024-11-14 21:52:51 -0300

    tela de instruções

--- jogo.py
+++ jogo.py
@@ -24,6 +24,8 @@ direita = True # Estabelece pra que lado o jogador está olhando no começo do j
 INIT = 0
 GAME = 1
 QUIT = 2
+INSTR = 3
+INSTR2 = 4
 # Define algumas variáveis com as cores básicas
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -365,12 +367,84 @@ def init_screen(screen):
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
-                        state = GAME
+                        state = INSTR
                         running = False
 
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(inicial, inicial_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
+
+def instr_screen(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+    
+    # Carrega o fundo da tela inicial
+    instr1 = pygame.image.load('assets/comojogar.png').convert()
+    instr1_rect = instr1.get_rect()
+    instr1 = pygame.transform.scale(instr1, (altura, largura))
+
+    running = True
+    while running:
+
+        # Ajusta a velocidade do jogo.
+        clock.tick(fps)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                        state = INSTR2
+                        running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(instr1, instr1_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
+
+def instr_screen2(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+    
+    # Carrega o fundo da tela inicial
+    instr2 = pygame.image.load('assets/comojogar2.png').convert()
+    instr2_rect = instr2.get_rect()
+    instr2 = pygame.transform.scale(instr2, (altura, largura))
+
+    running = True
+    while running:
+
+        # Ajusta a velocidade do jogo.
+        clock.tick(fps)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                        state = GAME
+                        running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(instr2, instr2_rect)
 
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
@@ -477,6 +551,10 @@ game = True
 while game:
     if state == INIT:
         state = init_screen(window)
+    elif state == INSTR:
+        state = instr_screen(window)
+    elif state == INSTR2:
+        state = instr_screen2(window)
     elif state == GAME:
         clock.tick(fps)
         for event in pygame.event.get():
@@ -521,7 +599,7 @@ while game:
         all_sprites.draw(window)
 
         # Desenha o score
-        pontos = assets['score_font'].render("{:08d}".format(score), True, YELLOW)
+        pontos = assets['score_font'].render("{:08d}".format(score), True, WHITE)
         text_rect = pontos.get_rect()
         text_rect.midtop = ((altura / 2),  45)
         window.blit(pontos, text_rect)
